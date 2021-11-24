2d89db3d354b3f89e958874df5700c1811a6ee2b
neoming <liyiming1998@qq.com>
2021-11-24 10:48:56 +0800

    day8

--- CPP/Solution.h
+++ CPP/Solution.h
@@ -32,6 +32,52 @@ struct TreeNode {
 
 class Solution {
 private:
+    int fib(int n) {
+        int dp [n+1];
+        if(n == 0)return 0;
+        if(n == 1)return 1;
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i]  = dp[i-1] + dp[i-2];
+            dp[i] %= 1000000007;
+        }
+        return dp[n];
+    }
+
+    int numWays(int n) {
+        int dp[n+1];
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] %= 1000000007;
+        }
+
+        return dp[n];
+    }
+
+    int maxProfit(std::vector<int>& prices) {
+        int min = INT_MAX;
+        int res = 0;
+        for(int& price : prices) {
+            if(price < min) min = price;
+            if(price - min > res) res = price - min;
+        }
+        return res;
+    }
+
     bool isSubStructure(TreeNode* A, TreeNode* B) {
         if( A == nullptr || B == nullptr) return false;
         if(A->val == B->val && isSub(A->right,B->right) && isSub(A->left,B->left))return true;
