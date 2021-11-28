67a975810bf200965c8f4373a38d4134e1e7b7df
neoming <liyiming1998@qq.com>
2021-11-28 21:28:03 +0800

    day12

--- CPP/Solution.h
+++ CPP/Solution.h
@@ -33,6 +33,45 @@ struct TreeNode {
 class Solution {
 private:
 
+    // day 12
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* cur = head;
+        while (l1 != nullptr && l2 != nullptr){
+            if(l1->val > l2->val){
+                cur->next = l2;
+                l2 = l2->next;
+            }else{
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            cur = cur->next;
+        }
+        while (l1 != nullptr){
+            cur->next = l1;
+            l1 = l1->next;
+            cur = cur->next;
+        }
+        while (l2 != nullptr){
+            cur->next = l2;
+            l2 = l2->next;
+            cur = cur->next;
+        }
+        return head->next;
+    }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == nullptr) return nullptr;
+        if(headB == nullptr) return nullptr;
+        ListNode* pa = headA;
+        ListNode* pb = headB;
+        while (pa != pb){
+            pa = pa == nullptr? headB : pa->next;
+            pb = pb == nullptr? headA : pb->next;
+        }
+        return pa;
+    }
+
     // day 11
     ListNode* deleteNode(ListNode* head, int val) {
         ListNode* prevNode = nullptr;
